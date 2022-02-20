26307cf53f505bafa147423669219e6f3256b2a3
BVAndonov <91953339+BVAndonov@users.noreply.github.com>
2022-02-20 21:19:56 +0200

    Update docker-setup.sh

--- HW02/docker-setup.sh
+++ HW02/docker-setup.sh
@@ -13,3 +13,16 @@ apt install -y docker-ce docker-ce-cli containerd.io
 
 echo "* Add vagrant user to docker group ..."
 usermod -aG docker vagrant
+
+echo "* Clone the dockerfile from a remote repository"
+git clone https://github.com/BVAndonov/DevOps_Course.git
+cp ~/DevOps_Course/HW02/Dockerfile ~/
+
+echo "* Clean up the unnecessary files"
+rm -rf  DevOps_Course/
+
+echo "* Build the image from the Dockerfile"
+docker image build -t apache /home/vagrant
+
+echo "* Run a container from the newlycreated image"
+docker container run -d -p 8080:80 apache
